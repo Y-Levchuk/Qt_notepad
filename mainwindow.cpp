0b39eb4fb8d81fb063fa6a26d06f0db82e92d506
Y-Levchuk <57503089+Y-Levchuk@users.noreply.github.com>
2019-11-26 23:33:43 +0200

    Qt_notepad

---
+++ mainwindow.cpp
@@ -0,0 +1,88 @@
+#include <vector>
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+static unsigned num;
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent),ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
+}
+MainWindow::~MainWindow() {delete ui;}
+static std::vector<QString> note,archive,home,just_note,com_a,com_b,com_c,proj2;
+
+void MainWindow::on_actionAdd_triggered() {note.push_back(ui->textEdit->toPlainText());}
+void MainWindow::on_actionNew_triggered() {ui->textEdit->setText(QString());}
+void MainWindow::on_actionAdd_to_archive_triggered()
+{
+    if(!note.empty()){
+    archive.push_back(note[num]);
+       auto tmp=note.begin();
+       for(unsigned i=0;i<num;i++)tmp++;
+       note.erase(tmp);
+    }
+}
+void MainWindow::on_actionShow_all_Note_triggered()
+{
+    QString tmp="";
+    for(auto a:note)tmp+=a+"\n____________________________________________\n";
+    ui->textEdit->setText(tmp);
+}
+void MainWindow::on_actionShow_archive_triggered()
+{
+    QString tmp="";
+    for(auto a:archive)tmp+=a+"\n____________________________________________\n";
+    ui->textEdit->setText(tmp);
+}
+void MainWindow::on_actionShow_note_triggered()   {if(!note.empty())ui->textEdit->setText(note[num]);}
+void MainWindow::on_actionAdd_in_home_triggered() {if(!note.empty())home.push_back(note[num]);}
+void MainWindow::on_actionproject2_triggered()    {if(!note.empty())proj2.push_back(note[num]);}
+void MainWindow::on_actionComponent_A_triggered() {if(!note.empty())com_a.push_back(note[num]);}
+void MainWindow::on_actionComponent_B_triggered() {if(!note.empty())com_b.push_back(note[num]);}
+void MainWindow::on_actionComponent_C_triggered() {if(!note.empty())com_c.push_back(note[num]);}
+void MainWindow::on_actionJust_note_triggered()   {if(!note.empty())just_note.push_back(note[num]);}
+void MainWindow::on_actionNext_triggered()        {if(!note.empty())ui->textEdit->setText(note[num<note.size()-1?++num:num]);}
+void MainWindow::on_actionBack_triggered()        {if(!note.empty())ui->textEdit->setText(note[num>0?--num:num]);}
+void MainWindow::on_actionDelete_note_triggered()
+{
+    if(!note.empty()){
+        auto tmp=note.begin();
+        for(unsigned i=0;i<num;i++)tmp++;
+        note.erase(tmp);
+        if(num>0)--num;
+    }
+}
+void MainWindow::on_actionShow_home_triggered()
+{
+    QString tmp="";
+    for(auto a:home)tmp+=a+"\n____________________________________________\n";
+    ui->textEdit->setText(tmp);
+}
+void MainWindow::on_actionShow_just_note_triggered()
+{
+    QString tmp="";
+    for(auto a:just_note)tmp+=a+"\n____________________________________________\n";
+    ui->textEdit->setText(tmp);
+}
+void MainWindow::on_actionshow_project2_triggered()
+{
+    QString tmp="";
+    for(auto a:proj2)tmp+=a+"\n____________________________________________\n";
+    ui->textEdit->setText(tmp);
+}
+void MainWindow::on_actionShow_component_A_triggered()
+{
+    QString tmp="";
+    for(auto a:com_a)tmp+=a+"\n____________________________________________\n";
+    ui->textEdit->setText(tmp);
+}
+void MainWindow::on_actionShow_component_B_triggered()
+{
+    QString tmp="";
+    for(auto a:com_b)tmp+=a+"\n____________________________________________\n";
+    ui->textEdit->setText(tmp);
+}
+void MainWindow::on_actionShow_component_C_triggered()
+{
+    QString tmp="";
+    for(auto a:com_c)tmp+=a+"\n____________________________________________\n";
+    ui->textEdit->setText(tmp);
+}
